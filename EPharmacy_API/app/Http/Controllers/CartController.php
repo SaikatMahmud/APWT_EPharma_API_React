c0cd2282e3d523a01e550e7427dbc175a440cdfd
Saikat Mahmud <saikat8486@gmail.com>
2022-08-17 01:09:06 +0600

    add to cart not working

--- EPharmacy_API/app/Http/Controllers/CartController.php
+++ EPharmacy_API/app/Http/Controllers/CartController.php
@@ -4,47 +4,87 @@ namespace App\Http\Controllers;
 
 use App\Models\EPCart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
+    // public function addToCart(Request $rq)
+    // { //adding to cart after click add to cart button
+    //     $rq->validate(
+    //         [
+    //             "quantity$rq->key" => "required|integer|max:$rq->avlQuantity",
+    //         ],
+    //         [
+    //             "quantity$rq->key.required" => "The quantity field is required.",
+    //             "quantity$rq->key.integer" => "Must be an decimal number",
+    //             "quantity$rq->key.max" => "More than avaiable stock",
+    //         ]
+    //     );
+    //     $cart = EPCart::where('customer_id', session()->get('loggedCustomer')->customer_id)
+    //         ->where('medicine_id', $rq->medId)->first();
+    //     if ($cart) { //update quantity if medicine already exist
+    //         EPCart::where('cart_id', $cart->cart_id)->update(
+    //             ['quantity' => $cart->quantity + $rq["quantity$rq->key"]]
+    //         );
+    //         session()->flash("added$rq->key", 'Medicine quantity updated');
+    //         return back();
+    //     }
+    //     //if not exist
+    //     $cart = new EPCart();
+    //     $cart->customer_id = session()->get('loggedCustomer')->customer_id;
+    //     $cart->medicine_id = $rq->medId;
+    //     $cart->quantity = $rq["quantity$rq->key"];
+    //     $cart->save();
+    //     if ($cart->save()) {
+    //         session()->flash("added$rq->key", 'Medicine added to cart');
+    //         return back();
+    //     } else {
+    //         session()->flash("added$rq->key", 'Add to cart failed');
+    //         return back();
+    //     }
+    // }
     public function addToCart(Request $rq)
     { //adding to cart after click add to cart button
-        $rq->validate(
+        $validator = Validator::make(
+            $rq->all(),
             [
-                "quantity$rq->key" => "required|integer|max:$rq->avlQuantity",
+                "quantity" => "required|integer|max:$rq->avlQuantity",
             ],
             [
-                "quantity$rq->key.required" => "The quantity field is required.",
-                "quantity$rq->key.integer" => "Must be an decimal number",
-                "quantity$rq->key.max" => "More than avaiable stock",
+                "quantity.required" => "The quantity field is required.",
+                "quantity.integer" => "Must be an decimal number",
+                "quantity.max" => "More than avaiable stock",
             ]
         );
-        $cart = EPCart::where('customer_id', session()->get('loggedCustomer')->customer_id)
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $cart = EPCart::where('customer_id', $rq->header("UserID"))
             ->where('medicine_id', $rq->medId)->first();
         if ($cart) { //update quantity if medicine already exist
             EPCart::where('cart_id', $cart->cart_id)->update(
-                ['quantity' => $cart->quantity + $rq["quantity$rq->key"]]
+                ['quantity' => $cart->quantity + $rq["quantity"]]
             );
-            session()->flash("added$rq->key", 'Medicine quantity updated');
+            session()->flash("added", 'Medicine quantity updated');
             return back();
         }
         //if not exist
         $cart = new EPCart();
-        $cart->customer_id = session()->get('loggedCustomer')->customer_id;
+        $cart->customer_id = $rq->header("UserID");
         $cart->medicine_id = $rq->medId;
-        $cart->quantity = $rq["quantity$rq->key"];
+        $cart->quantity = $rq["quantity"];
         $cart->save();
         if ($cart->save()) {
-            session()->flash("added$rq->key", 'Medicine added to cart');
+            session()->flash("added", 'Medicine added to cart');
             return back();
         } else {
-            session()->flash("added$rq->key", 'Add to cart failed');
+            session()->flash("added", 'Add to cart failed');
             return back();
         }
     }
-
-    public function removeFromCart($id){
-        EPCart::where('cart_id',$id)->delete();
+    public function removeFromCart($id)
+    {
+        EPCart::where('cart_id', $id)->delete();
         return redirect()->route('cus.cart');
     }
 }
--- epharmacy_react/src/components/customer/SearchResult.js
+++ epharmacy_react/src/components/customer/SearchResult.js
@@ -5,21 +5,18 @@ import Pagination from "react-js-pagination";
 import { createSearchParams, useSearchParams } from "react-router-dom";
 import Home from "./Home";
 import { useNavigate } from "react-router-dom";
-
 // require("bootstrap/less/bootstrap.less");
-
 //  import {Paginator} from "react-laravel-paginator";
 //  import { Pagination } from "react-laravel-paginex";
-
 const SearchResult = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const keyword = { search: searchParams.get("search") };
     const [result, setResult] = useState({});
-    const [loadPage, setLoadParams] = useState("");
     const [search, setSearch] = useState("");
     const [errs, setErrs] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [medId, setMedId] = useState("");
     const navigate = useNavigate();
-
 
     useEffect(() => {
         // axiosConfig.post("/search", keyword).then((rsp) => {
@@ -69,6 +66,16 @@ const SearchResult = () => {
             debugger
         })
     }
+    const addToCart = (quantity,medId,avlQuantity) => {
+        const data= {quantity:quantity,medid:medId,avlQuantity:avlQuantity}
+        axiosConfig.post("/add-to-cart",data).then((rsp) => {
+          debugger
+          setQuantity("");
+        }, (err) => {
+          setErrs(err.response.data);
+    
+        })
+      }
 
     // getData=(data)=>{
     //     axios.get('/search?page=' + data.page).then((response) => {
@@ -80,43 +87,56 @@ const SearchResult = () => {
             <br />
             <div align="right">
                 <form onSubmit={handleSubmit}>
-                    <input value={search} type="text" placeholder="Search here" onChange={(e) => { setSearch(e.target.value) }}></input>
+                    <input value={search} type="text" placeholder="Search here" onChange={(e) => { setSearch(e.target.value) }} />
                     <input type="submit" value="search" /><span> {errs ? errs : ''}</span>
                 </form>
             </div>
             <div>
                 {
-                    result.data?.map((med) =>
+                    result.data?.map((med, index) =>
                         // <li key={st.medicine_id}>{st.medicine_name}</li>
                         <span>
-                            <table border="1" align="center" cellpadding="10" width="40%">
-                                <td>
-                                    Name: {med.medicine_name}<br />
-                                    Genre: {med.genre}<br />
-                                    &emsp;&emsp;&emsp;
-                                    Details: see more...<br />
-                                    Price: {med.price} TK <br/>
-                                    <button>Add to cart</button>
-                                </td>
+                            {/* <form key={index} onSubmit={handleSubmit}> */}
+                                <table  border="1" align="center" cellpadding="10" width="40%">
+                                    <tbody key={index}>
+                                        <td key={index}>
+                                            Name: {med.medicine_name}<br />
+                                            Genre: {med.genre}<br />
+                                            &emsp;&emsp;&emsp;
+                                            Details: see more...<br />
+                                            Price: {med.price} TK <br />
+                                            <div align="right">
+                                            <input key={index}  value={quantity[index]} onChange={(e) => { setQuantity(e.target[index].value) }} type="number" placeholder="Quantity" />
+                                            Available:{med.availability}
+                                            {/* <input key={index} value={setMedId(med.medicine_id)} type="hidden"/> 
+                                    <input key={index} value={setMedId(med.medicine_id)} type="hidden"/>  */}
+                                           <br/> <button key={index} onClick={()=>addToCart(quantity,med.medicine_id,med.availability)}>Add to cart</button>
+                                            </div>
+                                            {errs.quantity ? errs.quantity[0] : ''}
+                                        </td>
 
-                            </table>
-                            &nbsp;
+                                    </tbody>
+                                </table>
+                                &nbsp;
+                            {/* </form> */}
                         </span>
+                                    // <input key={index} type="submit" value="search" /><span> {errs ? errs : ''}</span>
                     )
                 }
-                <div>
+
+                {/* <div>
                     <Pagination
                         activePage={result.current_page}
                         itemsCountPerPage={result.to}
                         totalItemsCount={result.total}
                         pageRangeDisplayed={5}
                         onChange={handlePageChange.bind(this)} />
-                </div>
+                </div> */}
 
                 {/* <Paginator currPage={result.current_page} lastPage={result.last_page} onChange={this.onCurrPageChange} /> */}
                 {/* <Pagination changePage={this.getData} data={data}/> */}
             </div >
-            
+
         </div>
     )
 
